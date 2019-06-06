2f1bdfc1f653b67a560a37a400154b00aacfdef3
AmyliaY <471816751@qq.com>
2019-06-06 22:57:10 +0800

    zookeeper的应用场景练习

---
+++ src/main/java/com/shuitu/application/javaapilock/LockWatcher.java
@@ -0,0 +1,24 @@
+package com.shuitu.application.javaapilock;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * author：水菟丸
+ */
+public class LockWatcher implements Watcher{
+
+    private CountDownLatch latch;
+
+    public LockWatcher(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void process(WatchedEvent event) {
+        if(event.getType()== Event.EventType.NodeDeleted){
+            latch.countDown();
+        }
+    }
+}
---
+++ src/main/java/com/shuitu/application/zkclient/UserCenter.java
@@ -0,0 +1,38 @@
+package com.shuitu.application.zkclient;
+
+import java.io.Serializable;
+
+/**
+ * author：水菟丸
+ */
+public class UserCenter implements Serializable{
+
+    private static final long serialVersionUID = -1776114173857775665L;
+    private int mc_id; //机器信息
+
+    private String mc_name;//机器名称
+
+    public int getMc_id() {
+        return mc_id;
+    }
+
+    public void setMc_id(int mc_id) {
+        this.mc_id = mc_id;
+    }
+
+    public String getMc_name() {
+        return mc_name;
+    }
+
+    public void setMc_name(String mc_name) {
+        this.mc_name = mc_name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCenter{" +
+                "mc_id=" + mc_id +
+                ", mc_name='" + mc_name + '\'' +
+                '}';
+    }
+}
